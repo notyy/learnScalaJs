d6d378a27da91ed4815a49de284e37712b258f63
notyy <notyycn@gmail.com>
2016-09-07 17:49:34 +0800

    add clickable button and javascript action to html

--- src/main/scala/webapp/LearningApp.scala
+++ src/main/scala/webapp/LearningApp.scala
@@ -1,10 +1,26 @@
 package webapp
 
 import scala.scalajs.js.JSApp
+import org.scalajs.dom
+import dom.document
+
+import scala.scalajs.js.annotation.JSExport
 
 object LearningApp extends JSApp{
-  @scala.scalajs.js.annotation.JSExport
+  @JSExport
   override def main(): Unit = {
-    println("hello,world")
+    appendPar(document.body, "Hello World")
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
+  }
+
+  @JSExport
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
   }
 }
