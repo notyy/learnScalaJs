bebc4cd6dba150ac8ee8ed83a25dd348fbab74e4
notyy <notyycn@gmail.com>
2016-09-08 14:14:22 +0800

    using jquery webjar

--- build.sbt
+++ build.sbt
@@ -6,6 +6,10 @@ scalaVersion := "2.11.8"
 
 scalaJSUseRhino in Global := false
 
+skip in packageJSDependencies := false
+jsDependencies +=
+  "org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js"
+
 libraryDependencies ++= Seq(
   "be.doeraene" %%% "scalajs-jquery" % "0.9.0"
 )
--- src/main/scala/webapp/LearningApp.scala
+++ src/main/scala/webapp/LearningApp.scala
@@ -1,26 +1,18 @@
 package webapp
 
-import scala.scalajs.js.JSApp
-import org.scalajs.dom
-import dom.document
+import org.scalajs.jquery.jQuery
 
+import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
 
 object LearningApp extends JSApp{
   @JSExport
   override def main(): Unit = {
-    appendPar(document.body, "Hello World")
-  }
-
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+    jQuery("body").append("<p>Hello World</p>")
   }
 
   @JSExport
   def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
+    jQuery("body").append("<p>You clicked the button!</p>")
   }
 }
