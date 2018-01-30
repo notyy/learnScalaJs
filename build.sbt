d994112b2ecc57681b010b39053230b12d80f684
notyy <notyycn@gmail.com>
2018-01-30 11:48:28 +0800

    jquery version runnable

--- build.sbt
+++ build.sbt
@@ -12,8 +12,7 @@ jsDependencies +=
   "org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js"
 
 libraryDependencies ++= Seq(
-  "org.scala-js" %%% "scalajs-dom" % "0.9.1"
-//  "be.doeraene" %%% "scalajs-jquery" % "0.9.0",
+  "be.doeraene" %%% "scalajs-jquery" % "0.9.1"
 //  "com.lihaoyi" %%% "utest" % "0.3.0" % "test",
 //  "org.scala-lang" % "scala-reflect" % "2.11.8"
 )
--- src/main/scala/webapp/TutorialApp.scala
+++ src/main/scala/webapp/TutorialApp.scala
@@ -1,24 +1,15 @@
 package webapp
 
-import org.scalajs.dom
-import dom.document
-
+import org.scalajs.jquery.jQuery
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 object TutorialApp {
   def main(args: Array[String]): Unit = {
-    appendPar(document.body, "Hello World")
-  }
-
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+    jQuery("body").append("<p>Hello World</p>")
   }
 
   @JSExportTopLevel("addClickedMessage")
   def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
+    jQuery("body").append("You clicked the button!")
   }
 }
